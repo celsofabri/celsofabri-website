efa6ce3c5f17ce31adeadf50bc0cee869d66671e
Celso Fabri Junior <celso.fabri@live.com>
2023-03-07 12:20:22 -0300

    feat: first commit

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Celso Fabri Junior`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "k_mVbL7yELUWwqFEfyi7OYBMx3HBqjOAKzyn1smILkw",
+      "spaceId": "3ojayojlocdr"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "UA-75770312-1"
+    }
+  }, "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
---
+++ src/containers/Works/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Works = () => {
+
+  return (
+    <h1>Trabalhos</h1>
+  )
+}
+
+export default Works;
